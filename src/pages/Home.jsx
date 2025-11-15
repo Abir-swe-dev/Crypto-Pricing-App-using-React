15e03c4a93d66c5d12934c3bd8d0594d43d370d4
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2025-11-15 12:44:26 +0600

    code commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,26 @@
+import { useEffect, useState } from "react";
 import { fetchCryptos } from "../api/coinGecko";
+
 export const Home = () => {
+   const [cryptoList,setCryptoList] = useState([]);
+   const [isLoading,setIsLoading] = useState(true);
+
+   useEffect(() => {
+      fetchCryptoData();
+   },[]);
+
  const fetchCryptoData = async () => {
-    const data = await fetchCryptos();
-    console.log(data);
- }
+
+   try {
+       const data = await fetchCryptos();
+      setCryptoList(data)
+   } catch (error) {
+      console.error("Error fetching crypto:",err);
+   } finally {
+      setIsLoading(false);
+   }
+   
+ };
+
  return <div>This is the home page</div>
 }
